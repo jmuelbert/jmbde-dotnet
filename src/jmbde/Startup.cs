84d39d305bd7a7112df134082eb908ba0fa7de37
Jürgen Mülbert <develop@juergen-muelbert.de>
2016-07-10 09:56:15 +0200

    Start with dotnet core.

--- src/jmbde/Startup.cs
+++ src/jmbde/Startup.cs
@@ -1,136 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.Data.Entity;
-using Microsoft.Extensions.PlatformAbstractions;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-
-using jmbde.Models;
+using Microsoft.AspNetCore.Builder;
 
 namespace jmbde
 {
     public class Startup
     {
-        /// <summary>
-        /// Default ctor
-        /// </summary>
-        /// <param name="env"></param>
-        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        public void Configure(IApplicationBuilder app)
         {
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-
-            if (env.IsDevelopment())
-            {
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
-                
-                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
-            }
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
-            Configuration["Data:DefaultConnection:ConnectionString"] = $@"Data Source={appEnv.ApplicationBasePath}/jmbdeSQLite.db3";
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         }
-
-        /// <summary>
-        /// The configuration Object
-        /// Static property as same instance is needed to be
-        /// returned across the project.
-        /// The instance will created when the generator
-        /// is called.
-        /// </summary>
-        public IConfigurationRoot Configuration { get; set; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            // *********************************
-            // SQLite Database
-            // Add EF services to the services
-            // container.
-            // *********************************
-            services.AddEntityFramework()
-                .AddSqlite()
-                .AddDbContext<JMBDEContext>(options =>
-                    options.UseSqlite(Configuration["Data:DefaultConnection:ConnectionString"]));
-
-
-            // Add ApplicationInsights
-            services.AddApplicationInsightsTelemetry(Configuration);
-
-            // Add framework services.
-            // Add MVC services to the services container
-            services.AddMvc();
-            // Add other services
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            // Configure the HTTP request pipeline.
-            // Add the console logger.
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseApplicationInsightsRequestTelemetry();
-
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                // Add Error handling middleware which catches all application specific errors and
-                // send the request to the following path or controller action.
-                app.UseExceptionHandler("/Home/Error");
-
-                // For more details on creating database during deployment see
-                // http://go.microsoft.con&fwlink/?LinkID=615859
-                try
-                {
-                    using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-                    {
-                        serviceScope.ServiceProvider.GetService<JMBDEContext>()
-                            .Database.Migrate();    
-                    }
-                }
-                catch (System.Exception exp)
-                {
-                    Console.WriteLine("Exception: {0}", exp); 
-                }
-            }
-
-            app.UseIISPlatformHandler(options => options.AuthenticationDescriptions.Clear());
-
-            app.UseApplicationInsightsExceptionTelemetry();
-
-            app.UseStaticFiles();
-            
-            // app.UseIdentity();
-            // app.UseDirectoryBrowser();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
--- src/jmbde/wwwroot/js/site.js
+++ src/jmbde/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿// Write your Javascript code.
+$(function () {
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item');
+
+    //Hack to make MVC ValidationSummary tag helper play nicely with Semantic UI
+    $('.ui.error ul').addClass('list');
+
+    if ($('.ui.error ul li:first').css('display') === 'none') {
+        $('.ui.error').hide();
+    } else {
+        $('.ui.error').show();
+    }
+
+    $('.ui.checkbox').checkbox();
+});
