a3e598b520ffb7d5b93651b5b52b98be6f131378
Jürgen Mülbert <JMuelbert@DE40170P.vw-group.com>
2017-08-24 11:48:35 +0200

    Employee: Add fieldchecks and fields for Edit.View

--- src/jmbde/Models/Employee.cs
+++ src/jmbde/Models/Employee.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2016 Jürgen Mülbert
+ * Copyright 2016, 2017 Jürgen Mülbert
  *
  * Licensed under the EUPL, Version 1.1 or – as soon they
    will be approved by the European Commission - subsequent
@@ -45,7 +45,7 @@ namespace jmbde.Models
         /// <summary>
         /// The Firstname
         /// </summary>
-        [StringLength(20), Display(Name = "Firstname")]
+        [StringLength(20, ErrorMessage = "First Name cannot be longer than 20 characters."), Display(Name = "Firstname")]
         public string FirstName { get; set; }
 
         /// <summary>
@@ -53,67 +53,72 @@ namespace jmbde.Models
         /// This is for future improvments
         /// actual dont need dont use
         /// </summary>
-        [StringLength(20), Display(Name = "Middle Name")]
+        [StringLength(20, ErrorMessage = "Middle Name cannot be longer than 20 characters."), Display(Name = "Middle Name")]
         public string MiddleName { get; set; }
 
         /// <summary>
         /// The Name
         /// This is <em>Required</em>
         /// </summary>
-        [Required, StringLength(30), Display(Name = "Last Name")]
+        [Required, StringLength(30, ErrorMessage = "Last Name cannot be longer than 30 characters."), Display(Name = "Last Name")]
         public string Name { get; set; }
 
         /// <summary>
         /// The Gender
         /// </summary>
-        [StringLength(1), Display(Name = "Gender")]
+        [StringLength(1, ErrorMessage = "Gender cannot be longer than 1 character."), Display(Name = "Gender")]
         public string Gender { get; set; }
 
         /// <summary>
         /// The Zipcode of the City (private)
         /// </summary>
-        [StringLength(10), Display(Name = "Zipcode")]
+        [StringLength(10, ErrorMessage = "Zipcode cannot be longer than 10 characters."), Display(Name = "Zipcode")]
         public string ZipCode { get; set; }
 
         /// <summary>
         /// The City
         /// </summary>
-        [StringLength(50), Display(Name = "City")]
+        [StringLength(50, ErrorMessage = "City Name cannot be longer than 50 characters."), Display(Name = "City")]
         public string City { get; set; }
 
         /// <summary>
         /// The Street
         /// </summary>
-        [StringLength(50), Display(Name = "Street")]
+        [StringLength(50, ErrorMessage = "Street Name cannot be longer than 50 characters."), Display(Name = "Street")]
         public string Street { get; set; }
 
         /// <summary>
         /// The PhoneNumber (private)
         /// </summary>
-        [StringLength(20), Display(Name = "Phonenumber")]
+        [StringLength(20, ErrorMessage = "Phone Number cannot be longer than 20 characters."), Display(Name = "Phonenumber")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
         /// <summary>
         /// The MobileNumber (private)
         /// </summary>
-        [StringLength(20), Display(Name = "Mobilenumber")]
+        [StringLength(20, ErrorMessage = "Mobile Number cannot be longer than 20 characters."), Display(Name = "Mobilenumber")]
+        [DataType(DataType.PhoneNumber)]
         public string MobileNumber { get; set; }
 
         /// <summary>
         /// The FaxNumber (private)
         /// </summary>
-        [StringLength(20), Display(Name = "Faxnumber")]
+        [StringLength(20, ErrorMessage = "Fax Number cannot be longer than 50 characters."), Display(Name = "Faxnumber")]
+        [DataType(DataType.PhoneNumber)]    
         public string FaxNumber { get; set; }
 
         /// <summary>
         /// The Mail Address
         /// </summary>
-        [StringLength(40), Display(Name = "E-Mail")]
+        [StringLength(40, ErrorMessage = "Mail Address cannot be longer than 50 characters."), Display(Name = "E-Mail")]
+        [DataType(DataType.EmailAddress)]
         public string Mail { get; set; }
 
         /// <summary>
         /// The Birthday
         /// </summary>
+        [DataType(DataType.Date)]
         public DateTime? Birthday { get; set; }
 
         //// <summary>
@@ -137,6 +142,8 @@ namespace jmbde.Models
         /// <summary>
         /// The Business Mail Address
         /// </summary>
+        [StringLength(40, ErrorMessage = "Business Mail Address cannot be longer than 50 characters."), Display(Name = "Business E-Mail")]
+        [DataType(DataType.EmailAddress)]
         public string BusinessMail { get; set; }
 
         /// <summary>
@@ -154,12 +161,14 @@ namespace jmbde.Models
         /// The StartDate
         /// from this date work the employee for the company
         /// </summary>
+        [DataType(DataType.Date)]
         public DateTime? StartDate { get; set; }
 
         /// <summary>
         /// The EndDate
         /// from this date work the employee for the company
         /// </summary>
+        [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; }
 
         /// <summary>
@@ -185,17 +194,20 @@ namespace jmbde.Models
         /// <summary>
         /// The ChipCard (Transponder) to access the areas
         /// </summary>
-        [StringLength(20), Display(Name = "Chip Card")]
+        [StringLength(20, ErrorMessage = "Chipcard Number cannot be longer than 20 characters."), Display(Name = "Chip Card")]
         public string ChipCard { get; set; }
 
         /// <summary>
         /// The Notes
         /// </summary>
+        [DataType(DataType.MultilineText)]
         public string Notes { get; set; }
 
         /// <summary>
         /// The Date and Time of last touch this DataSet
         /// </summary>
+        [DataType(DataType.Date)]
         public DateTime? LastUpdate { get; set; }
+
     }
 }
