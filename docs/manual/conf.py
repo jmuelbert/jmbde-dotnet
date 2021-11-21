261bb8d9069ca00731024628561c05b447661707
Jürgen <juergen.muelbert@gmail.com>
2021-11-21 14:08:48 +0100

    fix: change build.yml for building

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -59,7 +59,7 @@ if read_the_docs_build:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
+    "furo",
     "sphinx.ext.todo",
     "sphinx.ext.graphviz",
     "breathe",
