ff24d536a3e72b7401a7cc3b173a8048481500bb
Jürgen Mülbert <jmuelbert@users.noreply.github.com>
2021-11-27 14:00:28 +0100

    fix: resolve errors founded by linter.

--- docs/conf.py
+++ docs/conf.py
@@ -40,9 +40,9 @@ from datetime import datetime
 PROJECT = "jmbde-dotnet"
 AUTHOR = "Jürgen Mülbert"
 COPYRIGHT = f"{datetime.now().year}, {AUTHOR}"
-extensions = [
+EXTENSIONS = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
 ]
-autodoc_typehints = "description"
-html_theme = "furo"
+AUTODOC_TYPEHINTS = "description"
+HTML_THEME = "furo"
